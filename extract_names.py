25c33eaa12485c77f18d55b1ed1bbda2116938eb
Michael Hochleitner <hochleitner@protonmail.com>
2019-07-27 11:37:33 +0200

    Initial committ

---
+++ extract_names.py
@@ -0,0 +1,19 @@
+from nltk import word_tokenize, pos_tag, ne_chunk
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('words')
+nltk.download('words')
+
+with open('doesntwork.txt', 'r') as file: 
+    data = file.read()
+    data2 = data.split()
+    tokens = nltk.word_tokenize(data)
+    text = nltk.Text(tokens)
+
+def categorize_words():
+    print(pos_tag((tokens)))
+output = categorize_words()
+file = open("wordsfromdoesntwork.txt", "w")
+file.write(str(output))
+file.close()
